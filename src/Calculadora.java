8cbc1c8c34c492d620667088c4a75a8bba0a87c6
Andrijasevich Federico <fede_andrijasevich@hotmail.com>
2020-04-20 18:15:15 -0300

    Agrego variable tipo Scanner y se arreglo el Next()

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -7,15 +7,16 @@ public class Calculadora {
 		Integer b;
 		Integer opcion;
 		Integer resultado;
+		Scanner teclado = new Scanner(System.in);
 		do {
 			System.out.println("Ingrese su operacion /n 1 para sumar /n 2 para restar /3 para multiplicar /n 4 para dividir");
-			opcion = teclado.next();
+			opcion = teclado.nextInt();
 		}while(opcion < 1 && opcion > 4)
 		
 		System.out.println("Ingrese el primer numero");
-		a = teclado.next();
+		a = teclado.nextInt();
 		System.out.println("Ingrese el segundo numero");
-		a = teclado.next();
+		b = teclado.nextInt();
 		
 		switch(opcion){
 			
